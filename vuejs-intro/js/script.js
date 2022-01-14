5c6496ce616548d51a51682c3a02ec83dae5e60b
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2022-01-14 13:18:57 +0100

    vue if/else for

--- vuejs-intro/js/script.js
+++ vuejs-intro/js/script.js
@@ -11,7 +11,45 @@ new Vue({
     el: "#app",
     data: {
         message: '',
-        color: 'black'
+        color: 'black',
+        modalShow: false,
+        age: 0,
+        students: [
+            'Mino',
+            'Pino',
+            'Gino'
+        ],
+        currentIndex: 0,
+        freddure: [
+            {
+                titolo: 'Elettronica',
+                frase: 'Cosa fa un uccellino di un milligrammo su un ramo? Micro-cip! Micro-cip! Micro-cip!'
+            },
+            {
+                titolo: 'Il mare',
+                frase: 'Cosa fanno due squali in una gara? Vengono squalificati'
+            },
+            {
+                titolo: 'Fai da te!',
+                frase: 'Che vitaccia, ci vorrebbe un cacciavite più grosso.'
+            },
+            {
+                titolo: 'Colazione',
+                frase: 'Un uomo entra in un caffè...Splash'
+            },
+            {
+                titolo: 'Dieta',
+                frase: 'Ma perché se io mi nutro e tu ti nutri, Frank Sinatra?'
+            },
+            {
+                titolo: 'Psicologo',
+                frase: 'Dottore, soffro di personalità multipla… vi prego, aiutateci!'
+            },
+            {
+                titolo: 'Business',
+                frase: 'Chiude una fabbrica di carta igienica: andava a rotoli.'
+            }
+        ]
     },
     methods: {
         saluta: function () {
@@ -22,6 +60,25 @@ new Vue({
             // alle proprietà di data.
             alert(this.message)
         },
+        openModal: function () {
+            // this.modalShow === false
+            // if (!this.modalShow) { // qui è false
+            //     this.modalShow = true;
+            // } else {  // qui è true
+            //     this.modalShow = false;
+            // }
+            
+            // this.modalShow deve essere il contrario di sé stesso
+            this.modalShow = !this.modalShow;
+        },
+        nextFreddura: function () {
+            this.currentIndex++;
+            // todo -> e se arrivo alla fine dell'array?
+        },
+        prevFreddura: function () {
+            this.currentIndex--;
+            // todo -> e se arrivo a 0?
+        }
     }
 })
 
