89a57d3b398eed00862d2c84c6150470d53042ec
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2022-01-24 12:26:24 +0100

    lc axios

--- vuejs-axios/js/script.js
+++ vuejs-axios/js/script.js
@@ -7,16 +7,19 @@ new Vue({
         number: 0
     },
     methods: {
+        newNumber: function () {
+            // result.data è l'oggetto js (già trasformato da json a oggetto)
+            axios.get('https://flynn.boolean.careers/exercises/api/random/int').then((result) => {
+                console.log(result.data);
+                // prendiamo la response e salviamola in number
+                this.number = result.data.response
+            })
+        },
     },
     // quando l'applicazione è stata "montata" nel browser, fai una chiamata API
     mounted() {
         
-        // result.data è l'oggetto js (già trasformato da json a oggetto)
-        axios.get('https://flynn.boolean.careers/exercises/api/random/int').then((result) => {
-            console.log(result.data);
-            // prendiamo la response e salviamola in number
-            this.number = result.data.response
-        })
+        this.newNumber();
 
     }
 
