563e40213a6399957f3cf374b9d779ca035a5eba
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2022-01-24 12:24:03 +0100

    live coding axios

---
+++ vuejs-axios/js/script.js
@@ -0,0 +1,23 @@
+
+// Tramite una chiamata ad una API, stampiamo a schermo un numero casuale.
+
+new Vue({
+    el: "#app",
+    data: {
+        number: 0
+    },
+    methods: {
+    },
+    // quando l'applicazione è stata "montata" nel browser, fai una chiamata API
+    mounted() {
+        
+        // result.data è l'oggetto js (già trasformato da json a oggetto)
+        axios.get('https://flynn.boolean.careers/exercises/api/random/int').then((result) => {
+            console.log(result.data);
+            // prendiamo la response e salviamola in number
+            this.number = result.data.response
+        })
+
+    }
+
+})
